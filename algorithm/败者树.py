aaf933228a658ee60d84c1f6be9872bf807e8fe1
taoey <hwt8080@163.com>
2020-10-04 10:00:26 +0800

    FEATURE leetcode相关题目

--- algorithm/败者树.py
+++ algorithm/败者树.py
@@ -9,28 +9,31 @@ https://www.it610.com/article/1305779568971386880.htm
 K = 4  # 4路归并
 buf = []  # 存放多路归并的头元素，多出来的一位放最小值
 ls = []  # ls[0]保存的是胜者的下标，其余保存败者下标号
+MIN = -1  # 最小值要取对，保存在全部数中为最小
 
 
-def adjust(s):
+def adjust(index):
     """定义调整函数 s是buf数组的下标"""
-    t = (s + K) // 2  # 取s位的父节点
+    f_index = (index + K) // 2  # 取index的父节点
+    print("f_index:",f_index)
 
     # 如果当前节点不是根节点则进入下次循环，否则跳出
-    while t > 0:
-        # 比较当前节点值与父节点对应的值，如果父节点胜出则记录失败者并将当前节点换成胜利者
-        if buf[s] > buf[ls[t]]:
-            l = ls[t]
-            ls[t] = s
-            s = l
-        t = t // 2
+    while f_index > 0:
+        # 比较当前节点值与父节点对应的值，如果父节点胜出则记录失败者并将当前节点换成胜利者(当前规则：大者为败，小者为胜)
+        if buf[index] > buf[ls[f_index]]:
+            l = ls[f_index]
+            ls[f_index] = index
+            index = l
+        f_index = f_index // 2
     # 记录最终胜利者到ls[0]
-    ls[0] = s
+    ls[0] = index
+    print(buf,ls)
 
 
 def build():
     """定义败者树构建函数"""
     # 添加最小值-1到buf尾部
-    buf.append(-1)
+    buf.append(MIN)
 
     # 将败者树中间节点全部初始化为最小值所在位置即len(buf)-1
     for i in range(len(buf)):
@@ -40,16 +43,17 @@ def build():
     for i in range(K):
         adjust(i)
 
+
 def get_min():
     return buf[ls[0]]
 
+
 if __name__ == '__main__':
-    myarr = [12, 7, 6, 8]
-    buf = myarr
+    buf = [12, 10, 6, 8]
     build()
 
     print(get_min())
-    buf[2] = 1
+    buf[2] = -3
     adjust(2)
     print(get_min())
     pass
