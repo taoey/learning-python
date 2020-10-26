b3c75bcdea8df98663354f5728d2c0262f64b263
huangweitao <huangweitao@thunder.com.cn>
2020-10-26 17:45:24 +0800

    FEATURE 剑指 Offer 55 - I. 二叉树的深度

---
+++ algorithm/sword/剑指 Offer 55 - I. 二叉树的深度.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Definition for a binary tree node.
+import queue
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root: return 0
+        dep = 0
+        q = queue.Queue()
+        q.put(root)
+        while q.empty() is False:
+            nodes_len = q.qsize()
+            for i in range(nodes_len):
+                node = q.get()
+                if node.left: q.put(node.left)
+                if node.right: q.put(node.right)
+            dep += 1
+        return dep
+
+
+if __name__ == '__main__':
+    a = TreeNode(1)
+    b = TreeNode(2)
+    c = TreeNode(3)
+    d = TreeNode(4)
+    e = TreeNode(5)
+
+    a.left = b
+    a.right = c
+
+    b.left = d
+    c.right = e
+
+    s = Solution()
+    depth = s.maxDepth(a)
+    print(depth)
