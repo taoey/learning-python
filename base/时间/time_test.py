48048f70020e0affc2674989af11cf3ca8daad5b
huangweitao <huangweitao@thunder.com.cn>
2020-11-08 11:25:37 +0800

    PREF python时间使用

--- base/时间/time_test.py
+++ base/时间/time_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from datetime import datetime
-
+import datetime
 import pytest
 
 def test_01():
@@ -13,3 +13,21 @@ def test_01():
     table = "vip_order"
     sql = 'select distinct(ktv_id) from {} where state in (2, 8) and finish_time>="{}" and  finish_time<="{}"'.format(table,start_time,end_time)
     print(sql)
+
+def test_02():
+    str_p = '2019-01-30'
+    dateTime_p = datetime.datetime.strptime(str_p, '%Y-%m-%d')
+    print(dateTime_p)  # 2019-01-30 15:29:08
+
+
+def test_03():
+    str_p = '2019-01-30'
+    today = datetime.datetime.strptime(str_p, '%Y-%m-%d')
+    tomorow = today +  datetime.timedelta(days=1)
+
+    tomorow_p = datetime.datetime.strftime(tomorow, '%Y-%m-%d')
+    print(today,tomorow ,tomorow_p,type(tomorow),type(tomorow_p))
+
+    next = datetime.datetime.strptime("2019-01-31", '%Y-%m-%d')
+    if tomorow == next:
+        print("yes")
